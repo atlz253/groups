5d846f876389746e01bcfaef8558fd7fb6a0a9cb
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-03-12 19:18:00 +0300

    Добавил надпись при отсутствии групп

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -4,6 +4,10 @@ import { useGroups } from "../hooks/groups";
 function Groups() {
   const { groups } = useGroups();
 
+  if (groups.length === 0) {
+    return <div>Группы по данному запросу отсутствуют</div>;
+  }
+
   return (
     <>
       {groups.map((group) => (
